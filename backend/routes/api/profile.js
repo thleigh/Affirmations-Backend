e78ba2bc5586f839c897c520354d743b738da82a
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-10 15:45:52 -0700

    [cleanup]

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 // get route for user details
 // GET all quotes that a user commented on and that a user has liked
-
-
 router.get('/', (req, res) => {
     db.Users.findById()
     .then(response => {
